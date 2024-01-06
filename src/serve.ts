7e6ec6bc0d340d443c845726ab66501115d56110
J0aoCunha <jvjunger20123@gmail.com>
2024-01-06 00:36:39 -0300

    feat: creating application server

--- src/serve.ts
+++ src/serve.ts
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express();
 
-const port = 3333;
+app.use(express.json());  // Permite que o express entenda requisições com o corpo em JSON
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const port = 3333;  // Porta que o servidor vai ouvir
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));  // Inicia o servidor na porta 3333
