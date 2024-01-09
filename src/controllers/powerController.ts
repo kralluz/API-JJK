63a5056cf99cce54e4d948bf16982d1975bdcaf5
J0aoCunha <jvjunger20123@gmail.com>
2024-01-09 16:20:44 -0300

    feat: creating routes to list all powers and the route to list only 1

--- src/controllers/powerController.ts
+++ src/controllers/powerController.ts
@@ -2,10 +2,52 @@ import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import { z } from 'zod';
 
+
 const prisma = new PrismaClient();
 
 
-class powerController {}
+class powerController {
+
+	async listPowers(req:Request, res:Response) {
+
+		const allPowers = await prisma.power.findMany({
+			include: {
+				character: {
+					select:{
+						name:true
+					}
+				},
+			},
+		});
+
+		return res.status(200).json({allPowers});
+
+	}
+
+	async listPower(req:Request, res:Response) {
+		
+		const paramsSchema = z.object({
+			id: z.string().uuid(),
+		});
+
+		const { id } = paramsSchema.parse(req.params);
+
+		const power = await prisma.power.findUnique({
+			where: {
+				id,
+			},
+			include: {
+				character: {
+					select: {
+						name: true
+					}
+				},
+			}
+		});
+
+		return res.status(200).json({power});
+	}
+}
 
 
-export default new powerController();
+export default new powerController;
--- src/routes/powerRoutes.ts
+++ src/routes/powerRoutes.ts
@@ -6,3 +6,4 @@ export const powerRoutes = Router();
 
 
 powerRoutes.get('/powers', powerController.listPowers);  // Rota para listar todos os poderes
+powerRoutes.get('/power/:id', powerController.listPower);  // Rota para listar um poder
