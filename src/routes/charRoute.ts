811aa7adebd1c661a8cb3d449761f972f15e97a2
J0aoCunha <jvjunger20123@gmail.com>
2024-01-09 16:28:25 -0300

    feat: fixing character deletion route

--- src/routes/charRoute.ts
+++ src/routes/charRoute.ts
@@ -7,4 +7,4 @@ charRoute.get('/characters', charController.listChars);  // Rota para listar tod
 charRoute.get('/characters/:id', charController.listChar);  // Rota para listar um personagem
 charRoute.post('/character', charController.createChar); // Rota para criar um personagem
 charRoute.put('/character/:id', charController.updateChar); // Rota para atualizar um personagem
-charRoute.delete('/character:id', charController.deleteChar); // Rota para deletar um personagem
+charRoute.delete('/character/:id', charController.deleteChar); // Rota para deletar um personagem
