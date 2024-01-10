08114039f4b6cd8ae8f1d07d66aeab16cb33555d
J0aoCunha <jvjunger20123@gmail.com>
2024-01-10 20:03:19 -0300

    chore: config cors setup

--- src/serve.ts
+++ src/serve.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import { charRoute } from './routes/charRoute';
 import { powerRoutes } from './routes/powerRoutes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());  // Permite que o express entenda requisições com o corpo em JSON
 app.use(charRoute);  // Adiciona as rotas de characters ao servidor
 app.use(powerRoutes); // Adiciona as rotas de powers ao servidor
+app.use(cors({
+	credentials: true,
+	origin: 'http://localhost:3000'
+}));
 
 const port = 3333;  // Porta que o servidor vai ouvir
 
